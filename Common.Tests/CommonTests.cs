4d55c149263e621f7536a9b08eb8cdaa9618bd92
Mike Gatward <mike.gatward@alanblunden.co.uk>
2015-10-09 11:46:36 +0100

    Sort out Assemblyname and namespace as well as add a token test :D

---
+++ Common.Tests/CommonTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AlanBlunden.Common;
+
+namespace AlanBlunden.Common.Tests
+{
+    [TestClass]
+    public class CommonTests
+    {
+        [TestMethod]
+        public void TestHELLOReturnsHello()
+        {
+            Assert.AreEqual("hello", Common.HELLO);
+        }
+    }
+}
