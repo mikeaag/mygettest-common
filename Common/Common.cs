694a489271cab223261a7331beb23bae4db9e01d
Mike Gatward <mike.gatward@alanblunden.co.uk>
2015-10-09 09:59:05 +0100

    Update class name and namespace

--- Common/Common.cs
+++ Common/Common.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Common
+namespace AlanBlunden.Common
 {
-    public class Class1
+    public class Common
     {
     }
 }
